1e439c57c8bc714e7ad483f74e057134e4bb3f52
Peter WONG <luiges90@gmail.com>
2025-01-14 05:53:31 +0800

    add default template factions

--- src/com/luiges90/renx/RenxPlugin.java
+++ src/com/luiges90/renx/RenxPlugin.java
@@ -7,16 +7,7 @@ public class RenxPlugin extends BaseModPlugin {
     @Override
     public void onApplicationLoad() throws Exception {
         super.onApplicationLoad();
-
-        // Test that the .jar is loaded and working, using the most obnoxious way possible.
-        throw new RuntimeException("Template mod loaded!\nRemove this crash in TemplateModPlugin.");
+        System.out.println("ddd onApplicationLoad");
+        // com.fs.starfarer.api.Global.getSector().getAllFactions()
     }
-
-    @Override
-    public void onNewGame() {
-        super.onNewGame();
-        // Add your code here, or delete this method (it does nothing unless you add code)
-    }
-
-    // You can add more methods from ModPlugin here. Press Control-O in IntelliJ to see options.
 }
